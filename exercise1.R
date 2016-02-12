82a7e5cd1ab1f25d8d0326aaf28e8736a27341f8
Kush Gupta <bluek7@D-173-250-194-120.dhcp4.washington.edu>
2016-02-12 15:32:16 -0800

    Whatever

--- exercise1.R
+++ exercise1.R
@@ -13,14 +13,22 @@ d <- diamonds[sample(nrow(diamonds), 1000), ]
 # carat: horizontal position, 
 # price: vertical position, 
 # cut: color 
+#plot_ly(d, x = carat, y = price, mode = "markers", color = cut)
+plot_ly(d, x = carat, y = price, mode = "markers", color = cut, size = carat)
+
 
 # Pass your plot to the layout function to add a descriptive title
+plot_ly(d, x = carat, y = price, mode = "markers", color = cut, size = carat) %>% 
+  layout(title = "Graph about Diamonds (Comparing carat with price)")
 
 
 # Create another graph that compares diamond table to diamond depth, 
 # that also encodes information about price
+plot_ly(d, x = table, y = depth, mode = "markers", color = cut, size = price)
 
 # Pass your plot to the layout function to add a descriptive title
-
+plot_ly(d, x = depth, y = price, mode = "markers", 
+        color = price, size = price) %>% 
+  layout(title = "Graph about Diamonds (Comparing table with depth)")
 
 
